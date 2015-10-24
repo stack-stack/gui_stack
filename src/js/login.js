bff8548380e42bdd437110579b5665061234ecd6
JEREMY RIST <JTRAIN18@BRANDEIS.EDU>
2015-10-24 19:41:19 -0400

    Add ng-controller to html and clear the login for angular

--- src/js/login.js
+++ src/js/login.js
@@ -1,20 +1,4 @@
 ;(function(){
+  angular.module('gui', [ ])
 
- angular.module('gui', [ ])
-   .run(function($http, $rootScope){
-
-     $http.get('/api/stack.json')
-       .then(function (response){
-        //  $rootScope.login = response.data.login;
-         $rootScope.login = response.data;
-       })
-   })
-
-   .run(function($http, $rootScope){
-     $http.get('/api/stack.json')
-       .then(function (response){
-        //  $rootScope.login = response.data.login;
-         $rootScope.body = response.data;
-       })
-     })
-   })(); //END IIFE
+})(); //END IIFE
