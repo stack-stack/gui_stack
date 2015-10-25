32532e8bbeb86e04a60bfef5a1c094d8f0e3b1be
JEREMY RIST <JTRAIN18@BRANDEIS.EDU>
2015-10-24 20:06:32 -0400

    More building angular framework, adding directive and controller to the module

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,14 @@
 ;(function(){
   angular.module('gui', [ ])
 
+
+  main.directive('login', function(){
+    return {
+      templateUrl: 'login-page.html'
+    };
+
+    login.controller("LoginController", function(){
+
+    })
+  })
 })(); //END IIFE
