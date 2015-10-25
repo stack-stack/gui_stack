1d072d9702f2ca5eddca6581e4a776f1df0f7bc4
JEREMY RIST <JTRAIN18@BRANDEIS.EDU>
2015-10-25 10:27:31 -0400

    Attempt at using angular manipulating jmcreezys code from last night

--- src/js/login.js
+++ src/js/login.js
@@ -1,14 +1,40 @@
+// js for the index page
 ;(function(){
+
   angular.module('gui', [ ])
-
-
-  main.directive('login', function(){
-    return {
-      templateUrl: 'login-page.html'
+  .controller("login-Controller", function($scope, $http){
+    $scope.formvalues= {
+      name: "",
+      password: "",
+    }
+    $scope.submit= function(){
+      $http.post("/api/stack.json", $scope.formvalues)
+      .then(function (response){
+        console.log("can you hear me? please god hear me",response);
+      });
     };
-
-    login.controller("LoginController", function(){
-
-    })
   })
+    .run(function($http, $rootScope){
+      $http.get('/api/stack.json')
+      // $http.get('../questions.json')
+        .then(function (response){
+          console.log(arguments);
+          $rootScope.questions = response.data;
+          });
+        });
 })(); //END IIFE
+
+// ;(function(){
+//   angular.module('gui', [ ])
+//
+//
+//   main.directive('login', function(){
+//     return {
+//       templateUrl: 'login-page.html'
+//     };
+//
+//     login.controller("LoginController", function(){
+//
+//     })
+//   })
+// })(); //END IIFE
